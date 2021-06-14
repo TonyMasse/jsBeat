36ca3e2263330546da20b517bc432756a7dd5842
Tony Masse <tony.masse@logrhythm.com>
2021-06-14 21:23:45 +0100

    Code clean up

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 const LumberjackClient = require('lumberjack-client');
 const os = require('os');
 
-console.log('New Client...')
 const client = new LumberjackClient({
   host: 'localhost',
-  // host: '192.168.0.223',
   port: 5044,
-}, true)
+})
 
-
-console.log('Log...')
-client.log(
-  {
-    '@timestamp': new Date(),
-    '@metadata': {
-      beat: 'jsbeat',
-      version: '1.0.0'
-    },
-    host: {
-        hostname: os.hostname(),
-    },
-    message: 'hello world',
+function logMessage (msg) {
+  if (msg) {
+    client.log(
+      {
+        '@timestamp': new Date(),
+        '@metadata': {
+          beat: process.env.npm_package_name,
+          version: process.env.npm_package_version
+        },
+        host: {
+          hostname: os.hostname(),
+        },
+        message: msg,
+      }
+    )
   }
-)
+}
 
-console.log('Done.')
+logMessage('1: Hello World');
+logMessage();
+logMessage('3: Whoopsy!');
