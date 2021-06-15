243cd1dbcb1ca1f6cf7dd2e05bb69492bd4339bf
Tony Masse <tony.masse@logrhythm.com>
2021-06-15 18:32:24 +0100

    Move function to own file, use Config files, provide tail

--- src/index.js
+++ src/index.js
@@ -1,34 +1,8 @@
-const LumberjackClient = require('lumberjack-client');
-const os = require('os');
+// Load the logMessage function to push messages via Lumberjack to Open Collector
+const { logMessage } = require('./logMessage');
 
-const client = new LumberjackClient({
-  host: 'localhost',
-  port: 5044,
-})
-
-function logMessage (message, deviceType, filterHelpers) {
-  // message is the Object or String to push to the Open Collector
-  // deviceType is an optional String giving the specific of the device type (for example "myApp", "NetworkMonitor", "Mistnet", etc...)
-  // filterHelpers is an optional Object used by the Open Collector filter to include or exclude the message (for example: { filter_abc: true, filter_xyz: false } )
-
-  if (message) {
-    client.log(
-      {
-        '@timestamp': new Date(),
-        '@metadata': {
-          beat: process.env.npm_package_name,
-          version: process.env.npm_package_version,
-          device_type: deviceType,
-          filter_helpers: filterHelpers
-        },
-        host: {
-          hostname: os.hostname(),
-        },
-        message: message,
-      }
-    )
-  }
-}
+// Get command line params
+const commandArgs = process.argv.slice(2);
 
 logMessage('1: Hello World');
 // { "@metadata": { "beat": "jsbeat", "version": "1.0.0" }, "@timestamp": "2021-06-14T21:04:02.192Z", "host": { "hostname": "ip-192-168-100-12.eu-west-1.compute.internal" }, "message": "1: Hello World" }
@@ -45,3 +19,24 @@ logMessage('6: Daisy!', 'myApp');
 logMessage('7: Daisy!', 'myApp', { doggy: 'Good boy', filter_xyz: true });
 // { "@metadata": { "beat": "jsbeat", "device_type": "myApp", "filter_helpers": { "doggy": "Good boy", "filter_xyz": true }, "version": "1.0.0" }, "@timestamp": "2021-06-14T21:04:02.192Z", "host": { "hostname": "ip-192-168-100-12.eu-west-1.compute.internal" }, "message": "7: Daisy!" }
 
+if (commandArgs && commandArgs[0] && commandArgs[0].length) {
+  console.log('Tailing: ' + commandArgs[0] + '...');
+
+  const Tail = require('tail').Tail;
+
+  try {
+    tail = new Tail(commandArgs[0]);
+
+    tail.on("line", function (data) {
+      console.log(data);
+      logMessage(data);
+    });
+
+    tail.on("error", function (error) {
+      console.log('ERROR: ', error);
+    });
+
+  } catch (err) {
+    console.error(err);
+  }
+}
---
+++ src/logMessage.js
@@ -0,0 +1,42 @@
+const LumberjackClient = require('lumberjack-client');
+const os = require('os');
+
+// Get Lumberjack config
+const fs = require('fs');
+const path = require('path');
+
+const lumberjackConfig = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'config', 'lumberjack.json'), 'utf8'));
+
+const client = new LumberjackClient({
+  host: (lumberjackConfig && lumberjackConfig.host && lumberjackConfig.host.length ? lumberjackConfig.host : 'localhost'),
+  port: (lumberjackConfig && lumberjackConfig.port && lumberjackConfig.port.length ? lumberjackConfig.port : 5044),
+})
+
+function logMessage (message, deviceType, filterHelpers) {
+  // message is the Object or String to push to the Open Collector
+  // deviceType is an optional String giving the specific of the device type (for example "myApp", "NetworkMonitor", "Mistnet", etc...)
+  // filterHelpers is an optional Object used by the Open Collector filter to include or exclude the message (for example: { filter_abc: true, filter_xyz: false } )
+
+  if (message) {
+    console.log('🌠');
+    client.log(
+      {
+        '@timestamp': new Date(),
+        '@metadata': {
+          beat: process.env.npm_package_name,
+          version: process.env.npm_package_version,
+          device_type: deviceType,
+          filter_helpers: filterHelpers
+        },
+        host: {
+          hostname: os.hostname(),
+        },
+        message: message,
+      }
+    )
+  }
+}
+
+module.exports = {
+  logMessage
+}
