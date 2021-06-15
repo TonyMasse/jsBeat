e21279574c19bbe801ce4dcae43b684c819e78cc
Tony Masse <tony.masse@logrhythm.com>
2021-06-15 23:19:21 +0100

    FlatFileReader class

--- src/index.js
+++ src/index.js
@@ -1,42 +1,49 @@
 // Load the logMessage function to push messages via Lumberjack to Open Collector
-const { logMessage } = require('./logMessage');
+const { logMessage } = require('./outputs/logMessage');
+const { FlatFileReader } = require('./inputs/flatFile');
 
 // Get command line params
 const commandArgs = process.argv.slice(2);
 
-logMessage('1: Hello World');
-// { "@metadata": { "beat": "jsbeat", "version": "1.0.0" }, "@timestamp": "2021-06-14T21:04:02.192Z", "host": { "hostname": "ip-192-168-100-12.eu-west-1.compute.internal" }, "message": "1: Hello World" }
-logMessage(); // << this will NOT log anything
-logMessage(''); // << this neither
-logMessage('3: Whoopsy!');
-// { "@metadata": { "beat": "jsbeat", "version": "1.0.0" }, "@timestamp": "2021-06-14T21:04:02.192Z", "host": { "hostname": "ip-192-168-100-12.eu-west-1.compute.internal" }, "message": "3: Whoopsy!" }
-logMessage({ id: 4, abc: 'DEF' });
-// { "@metadata": { "beat": "jsbeat", "version": "1.0.0" }, "@timestamp": "2021-06-14T21:02:58.275Z", "host": { "hostname": "ip-192-168-100-12.eu-west-1.compute.internal" }, "message": { "abc": "DEF", "id": 4 } }
-logMessage('5: Daisy!');
-// { "@metadata": { "beat": "jsbeat", "version": "1.0.0" }, "@timestamp": "2021-06-14T21:02:58.275Z", "host": { "hostname": "ip-192-168-100-12.eu-west-1.compute.internal" }, "message": "5: Daisy!" }
-logMessage('6: Daisy!', 'myApp');
-// { "@metadata": { "beat": "jsbeat", "device_type": "myApp", "version": "1.0.0" }, "@timestamp": "2021-06-14T21:02:58.275Z", "host": { "hostname": "ip-192-168-100-12.eu-west-1.compute.internal" }, "message": "6: Daisy!" }
-logMessage('7: Daisy!', 'myApp', { doggy: 'Good boy', filter_xyz: true });
-// { "@metadata": { "beat": "jsbeat", "device_type": "myApp", "filter_helpers": { "doggy": "Good boy", "filter_xyz": true }, "version": "1.0.0" }, "@timestamp": "2021-06-14T21:04:02.192Z", "host": { "hostname": "ip-192-168-100-12.eu-west-1.compute.internal" }, "message": "7: Daisy!" }
+// Some examples of use:
+// logMessage('1: Hello World');
+// // { "@metadata": { "beat": "jsbeat", "version": "1.0.0" }, "@timestamp": "2021-06-14T21:04:02.192Z", "host": { "hostname": "ip-192-168-100-12.eu-west-1.compute.internal" }, "message": "1: Hello World" }
+// logMessage(); // << this will NOT log anything
+// logMessage(''); // << this neither
+// logMessage('3: Whoopsy!');
+// // { "@metadata": { "beat": "jsbeat", "version": "1.0.0" }, "@timestamp": "2021-06-14T21:04:02.192Z", "host": { "hostname": "ip-192-168-100-12.eu-west-1.compute.internal" }, "message": "3: Whoopsy!" }
+// logMessage({ id: 4, abc: 'DEF' });
+// // { "@metadata": { "beat": "jsbeat", "version": "1.0.0" }, "@timestamp": "2021-06-14T21:02:58.275Z", "host": { "hostname": "ip-192-168-100-12.eu-west-1.compute.internal" }, "message": { "abc": "DEF", "id": 4 } }
+// logMessage('5: Daisy!');
+// // { "@metadata": { "beat": "jsbeat", "version": "1.0.0" }, "@timestamp": "2021-06-14T21:02:58.275Z", "host": { "hostname": "ip-192-168-100-12.eu-west-1.compute.internal" }, "message": "5: Daisy!" }
+// logMessage('6: Daisy!', 'myApp');
+// // { "@metadata": { "beat": "jsbeat", "device_type": "myApp", "version": "1.0.0" }, "@timestamp": "2021-06-14T21:02:58.275Z", "host": { "hostname": "ip-192-168-100-12.eu-west-1.compute.internal" }, "message": "6: Daisy!" }
+// logMessage('7: Daisy!', 'myApp', { doggy: 'Good boy', filter_xyz: true });
+// // { "@metadata": { "beat": "jsbeat", "device_type": "myApp", "filter_helpers": { "doggy": "Good boy", "filter_xyz": true }, "version": "1.0.0" }, "@timestamp": "2021-06-14T21:04:02.192Z", "host": { "hostname": "ip-192-168-100-12.eu-west-1.compute.internal" }, "message": "7: Daisy!" }
+
+// List of the active Inputs and Outputs
+const inputs = [];
+const outputs = [];
 
 if (commandArgs && commandArgs[0] && commandArgs[0].length) {
   console.log('Tailing: ' + commandArgs[0] + '...');
 
-  const Tail = require('tail').Tail;
+  const deviceType = (commandArgs[1] && commandArgs[1].length ? commandArgs[1] : undefined)
 
-  try {
-    tail = new Tail(commandArgs[0]);
-
-    tail.on("line", function (data) {
-      console.log(data);
-      logMessage(data);
-    });
-
-    tail.on("error", function (error) {
-      console.log('ERROR: ', error);
-    });
-
-  } catch (err) {
-    console.error(err);
-  }
+  inputs.push({
+    type: 'flatFile',
+    name: deviceType || commandArgs[0],
+    handler: new FlatFileReader({
+      path: commandArgs[0],
+      autoStart: true,
+      printToConsole: true,
+      deviceType,
+      filterHelpers: {
+        flatFile: true,
+        filePath: commandArgs[0],
+        logSourceType: 'Flat File'
+      }
+    },
+    logMessage)
+  });
 }
---
+++ src/inputs/flatFile.js
@@ -0,0 +1,90 @@
+'use strict';
+const Tail = require('tail').Tail;
+
+class FlatFileReader {
+
+  constructor(config, loggerFunction) {
+    // config: object containing these params:
+    // - path: string. Full path to the log file to tail. Must be non-empty.
+    // - autoStart: boolean. If false, it will only create the object and wait for start() to be called. Otherwise (default) it will try to start capturing the data immediately.
+    // - printToConsole: boolean. It true, it will print out to the Console, as well as to the Open Collector.
+    // - deviceType: string. The name of the Device Type, to pass onto the Open Collector Pipeline.
+    // - filterHelpers: object. A set of flags/strings/objects to help the JQ filter of the Open Collector Pipeline to trigger on.
+
+    Object.assign(this, {
+      config: { ...config },
+      state: {
+        hasFailedToUseLogger: false
+        // Ideally we would need file position too, but we are starting with a crude version for now
+      }
+    })
+
+    if (loggerFunction) {
+      this.use(loggerFunction);
+    }
+
+    if (config.autoStart !== false) {
+      this.start();
+    }
+  } // constructor
+
+  use (loggerFunction) {
+    // loggerFunction must have the following parameters:
+    // (message, deviceType, filterHelpers)
+    // - message is the Object or String to push to the Open Collector
+    // - deviceType is an optional String giving the specific of the device type (for example "myApp", "NetworkMonitor", "Mistnet", etc...)
+    // - filterHelpers is an optional Object used by the Open Collector filter to include or exclude the message (for example: { filter_abc: true, filter_xyz: false } )
+    if (loggerFunction && typeof loggerFunction === 'function') {
+      this.loggerFunction = loggerFunction
+    } else {
+      const err = new Error('loggerFunction must be a valid function.')
+      console.error(err);
+    }
+  } // use
+
+  start () {
+    if (this.config.path && this.config.path.length) {
+      if (this.loggerFunction && typeof this.loggerFunction === 'function') {
+        try {
+          this.tail = new Tail(this.config.path);
+
+          this.tail.on("line", this.onTailLineEvent.bind(this));
+
+          this.tail.on("error", function (error) {
+            console.log('ERROR: ', error);
+          });
+
+        } catch (err) {
+          console.error(err);
+        }
+      } else {
+        const err = new Error('loggerFunction must be a valid function.')
+        console.error(err);
+      }
+    } else {
+      const err = new Error('path must be a non empty string.')
+      console.error(err);
+    }
+  } // start
+
+  onTailLineEvent (data) {
+    if (this.config.printToConsole === true) {
+      console.log(data);
+    }
+    try {
+      this.loggerFunction(data, this.config.deviceType, this.config.filterHelpers);
+    } catch (err) {
+      // Fails once with an error, then silently
+      if (this.state.hasFailedToUseLogger !== true) {
+        this.state.hasFailedToUseLogger = true;
+        const err = new Error('Failed to send line to Open Collector via Lumberjack.')
+        console.log('ERROR: ', err);
+      }
+    }
+  }
+}
+
+
+module.exports = {
+  FlatFileReader
+};
