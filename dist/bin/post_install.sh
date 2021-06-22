b707f4204d1148846fbbc7834bd1b4591ef501ff
Tony Masse <tony.masse@logrhythm.com>
2021-06-22 23:12:09 +0100

    Proper start.sh script, add post_install.sh script

---
+++ dist/bin/post_install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+readonly JSBEAT_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")"/../ && pwd)
+
+readonly SERVICE_FILE="/usr/lib/systemd/system/jsbeat.service"
+readonly SERVICE_LINK="/etc/systemd/system/jsbeat.service"
+
+if ! [ -f "$JSBEAT_ROOT/bin/start.sh" ];
+then
+echo -e "ERROR: $JSBEAT_ROOT/bin/start.sh not found. Exiting."
+exit 42
+fi
+
+chmod +x "$JSBEAT_ROOT/bin/start.sh"
+
+if [ -f "$SERVICE_FILE" ];
+then
+echo -e "$SERVICE_FILE already exist."
+else
+echo -e "Creating $SERVICE_FILE."
+cat > "$SERVICE_FILE" <<- EOM
+[Unit]
+Description=A light, Javascript based, Beat to collect data for the Open Collector
+Documentation=https://github.com/TonyMasse/jsBeat
+Wants=network-online.target
+After=network-online.target
+StartLimitIntervalSec=0
+
+[Service]
+ExecStart=/opt/jsBeat/bin/start.sh
+Restart=always
+RestartSec=5
+Type=simple
+
+[Install]
+WantedBy=multi-user.target
+EOM
+
+fi
+
+if [ -e "$SERVICE_LINK" ];
+then
+echo -e "$SERVICE_LINK already exist."
+else
+echo -e "Creating symbolic link: $SERVICE_LINK -> $SERVICE_FILE."
+ln -s "$SERVICE_FILE" $SERVICE_LINK
+fi
--- dist/bin/start.sh
+++ dist/bin/start.sh
@@ -1,3 +1,12 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-node main.js
+readonly JSBEAT_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")"/../ && pwd)
+
+if [ -f "$JSBEAT_ROOT/lib/node/bin/node" ];
+then
+# Use the bundled NodeJS
+"$JSBEAT_ROOT/lib/node/bin/node" "$JSBEAT_ROOT/bin/main.js"
+else
+# Use the system-wise NodeJS (if any)
+node "$JSBEAT_ROOT/bin/main.js"
+fi
