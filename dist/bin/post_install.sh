a466a2b6cc0a1c38bb74b4df3efea85493925645
Tony Masse <tony.masse@logrhythm.com>
2021-06-23 18:13:32 +0100

    Update post_install script

--- dist/bin/post_install.sh
+++ dist/bin/post_install.sh
@@ -5,6 +5,8 @@ readonly JSBEAT_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")"/../ && pwd)
 readonly SERVICE_FILE="/usr/lib/systemd/system/jsbeat.service"
 readonly SERVICE_LINK="/etc/systemd/system/jsbeat.service"
 
+echo -e "jsBeat - Creating Service...\n"
+
 if ! [ -f "$JSBEAT_ROOT/bin/start.sh" ];
 then
 echo -e "ERROR: $JSBEAT_ROOT/bin/start.sh not found. Exiting."
@@ -15,7 +17,7 @@ chmod +x "$JSBEAT_ROOT/bin/start.sh"
 
 if [ -f "$SERVICE_FILE" ];
 then
-echo -e "$SERVICE_FILE already exist."
+echo -e "$SERVICE_FILE already exist. Leaving as is."
 else
 echo -e "Creating $SERVICE_FILE."
 cat > "$SERVICE_FILE" <<- EOM
@@ -40,7 +42,7 @@ fi
 
 if [ -e "$SERVICE_LINK" ];
 then
-echo -e "$SERVICE_LINK already exist."
+echo -e "$SERVICE_LINK already exist. Leaving as is."
 else
 echo -e "Creating symbolic link: $SERVICE_LINK -> $SERVICE_FILE."
 ln -s "$SERVICE_FILE" $SERVICE_LINK
