73c5dd4b3a1f7c9776d43b38af96423c57ad0111
Tony Masse <tony.masse@logrhythm.com>
2021-07-02 17:25:35 +0100

    Move minimified JS to dist/jsBeat/bin/main.js and produced TGZ releases to dist/

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.join(__dirname, 'dist', 'bin'),
+        path: path.join(__dirname, 'dist', 'jsBeat', 'bin'),
         filename: 'main.js'
     },
     mode: 'production',
